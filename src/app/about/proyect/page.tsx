4345e7b608d19a5d8f3b998fbfc143d0c395cded
carobarragan <104798095+carobarragan@users.noreply.github.com>
2023-12-05 10:38:17 -0300

    footer

--- src/app/about/proyect/page.tsx
+++ src/app/about/proyect/page.tsx
@@ -6,43 +6,45 @@ export default function Proyect() {
   return (
     <div>
       <h1 className={Style.titulo}>Proyectos</h1>
-
       <h2 className={Style.titulo2}>Formulario</h2>
       <p className={Style.informacion}>
         Formulario de contacto con conexión a base de datos + vercel ship. en
         este formulario utiliza Sonner una biblioteca de React para mostrar la
         notificacion del mensaje junto a un confetti{" "}
       </p>
-      <img className={Style.imagen} src="/formulario.png"></img>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        {" "}
-        <a href="https://formulario-wheat-one.vercel.app/" />
+      <img className={Style.imagen} src="/formulario.png"></img>{" "}
+      <a
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        href="https://formulario-wheat-one.vercel.app/"
+      >
         Clic Aqui
-      </button>
+      </a>
       <h2 className={Style.titulo2}>Juego Tic-Tac</h2>
       <p className={Style.informacion}>
         {" "}
         Card de juego, realizado con React + Css; quien no lo ha jugado :)
       </p>
       <img className={Style.imagen} src="/juego.png"></img>
-      <img className={Style.imagen} src="/juego1.png "></img>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        {" "}
-        <a href="https://precious-cendol-88d048.netlify.app/" />
+      <img className={Style.imagen} src="/juego1.png "></img>{" "}
+      <a
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        href="https://precious-cendol-88d048.netlify.app/"
+      >
         Clic Aqui
-      </button>
+      </a>
       <h2 className={Style.titulo2}> Pagina de Servicio de Moza</h2>
       <p className={Style.informacion}>
         {" "}
         Pagina de servicio realizada con Next.js, que es un framework de React +
         css
       </p>
-      <img className={Style.imagen} src="/SM.jpg"></img>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        {" "}
-        <a href="https://moza-servicio.vercel.app/" />
+      <img className={Style.imagen} src="/SM.jpg"></img>{" "}
+      <a
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        href="https://moza-servicio.vercel.app/"
+      >
         Clic Aqui
-      </button>
+      </a>
       <h2 className={Style.titulo2}>Proyecto Grupal</h2>
       <p className={Style.informacion}>
         Es una aplicación de viaje, donde cuenta con tres perfiles;
@@ -51,11 +53,12 @@ export default function Proyect() {
         la adquisición del servicio.
       </p>
       <img className={Style.imagen} src="/pg.jpeg"></img>
-      <button className=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        {" "}
-        <a href="https://wetravel-app.vercel.app/" />
+      <a
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        href="https://wetravel-app.vercel.app/"
+      >
         Clic Aqui
-      </button>
+      </a>
     </div>
   );
 }
